8dbf5621928bad628a5f61c83fa13b441d2d0e06
Andrii Kulyk <kyluk5@gmail.com>
2020-06-02 21:01:33 +0300

    second_done-second-time

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// import "./tasks/task_1.js";
+import "./tasks/task_1.js";
 import "./tasks/task_2.js";
 // import "./tasks/task_3.js";
 //  import './tasks/task_4.js'
--- tasks/task_2.js
+++ tasks/task_2.js
@@ -8,13 +8,23 @@ const ingredients = [
   "Зелень",
   "Приправы",
 ];
+// ===================================
+// const createEl = ingredients
+//   .map((item) => {
+//     return `<li>${item}</li>`;
+//   })
+//   .join("");
 
-const createEl = ingredients
-  .map((item) => {
-    return `<li>${item}</li>`;
-  })
-  .join("");
+// const searchUl = document.querySelector("#ingredients");
+// searchUl.innerHTML = createEl;
+// console.log(searchUl);
+// ===================================
+const createRef = ingredients.map((item) => {
+  let li = document.createElement("li");
+  li.textContent = item;
+  return li;
+});
 
 const searchUl = document.querySelector("#ingredients");
-searchUl.innerHTML = createEl;
+searchUl.append(...createRef);
 console.log(searchUl);
