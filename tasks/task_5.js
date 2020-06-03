cc574370b07c1be0736d407380a5a92a3412f960
Andrii Kulyk <kyluk5@gmail.com>
2020-06-03 15:03:09 +0300

    fifth_hope-done

--- tasks/task_5.js
+++ tasks/task_5.js
@@ -1,13 +1,12 @@
 // debugger;
 
 const inputEl = document.querySelector("#name-input");
-// console.log(inputEl);
-inputEl.addEventListener("input", addText);
-
-function addText(event) {
-  //   console.log(event.currentTarget.value);
-  spanEl.textContent = event.currentTarget.value;
-}
-
 const spanEl = document.querySelector("#name-output");
-// console.log(spanEl);
+
+inputEl.addEventListener("input", addText);
+function addText(event) {
+  spanEl.textContent = event.target.value;
+  if (event.target.value === "") {
+    return (spanEl.textContent = "незнакомец");
+  }
+}
