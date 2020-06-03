5d019981eeb110a749a3058a6a73ffdb8bddcf2c
Andrii Kulyk <kyluk5@gmail.com>
2020-06-03 16:21:02 +0300

    fifth_almoust-done

--- tasks/task_6.js
+++ tasks/task_6.js
@@ -1,1 +1,11 @@
 const validationInput = document.querySelector("#validation-input");
+// console.dir(validationInput);
+
+validationInput.addEventListener("change", validationFn);
+function validationFn(even) {
+  if (even.target.selectionStart < even.target.dataset.length) {
+    validationInput.classList.add("invalid");
+  } else if (even.target.selectionStart > even.target.dataset.length) {
+    validationInput.classList.add("valid");
+  }
+}
